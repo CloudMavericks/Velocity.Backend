4266170cbd4d0c377f1faad54ab8f1a04c0214e3
SathiyaramanM <Sathiyaraman2003@gmail.com>
2023-09-09 14:54:12 +0530

    Setup services and middleware in Program.cs

--- src/Velocity.API/Program.cs
+++ src/Velocity.API/Program.cs
@@ -1,6 +1,38 @@
+using Microsoft.OpenApi.Models;
+using Velocity.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+builder.Services.AddTokenConfiguration(builder.Configuration);
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Velocity.API", Version = "v1" });
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Velocity!");
+app.UseCors();
 
-app.Run();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Velocity.API v1"));
+
+await app.RunAsync();
